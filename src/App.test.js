303d15f1c5e5070397fb2e8eab3fe9a4cbff88d3
Emily Yu <bluerainmango@gmail.com>
2021-09-03 00:40:02 -0700

    added second test, button element

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
 
+  // access to DOM
   // getByRole is recommended over getById for accessibility.
   // name: innerText
   const colorBtn = screen.getByRole("button", { name: "Change to blue" });
 
-  // toHaveStyle(): jest-dom's method
+  // assertion. always start with expect. toHaveStyle(): jest-dom's method
   expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("button turns blue when clicked", () => {
+  render(<App />);
+
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+
+  expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorBtn);
+
+  expect(colorBtn).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to red'
+  expect(colorBtn.textContent).toBe("Change to red");
 });
