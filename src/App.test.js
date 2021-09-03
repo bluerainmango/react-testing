5a855216725f0891e361920e2232e1980c0fb359
Emily Yu <bluerainmango@gmail.com>
2021-09-02 23:34:18 -0700

    first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // getByRole is recommended over getById for accessibility.
+  // name: innerText
+  const colorBtn = screen.getByRole("button", { name: "Change to blue" });
+
+  // toHaveStyle(): jest-dom's method
+  expect(colorBtn).toHaveStyle({ backgroundColor: "red" });
 });
